343b920ff6aca4b27a14920c3b4d6fef5acc3578
Samir <samir@lafabriqueaclients.fr>
2023-07-04 11:08:35 +0200

    update

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+cloud {
+    organization = "terra_sam"
+
+    workspaces {
+      name = "terraform_lab"
+    }
+  }
+ 
+  }
+
+
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "continuous-delivery-with-git"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
